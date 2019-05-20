a3ca2796718b0b595a41ff897f61e010304cde1a
Truong <32908615+HoangDuyTruong@users.noreply.github.com>
2019-05-20 16:34:10 +0700

    them form cho can bo
    them form danh sach can bo

---
+++ Source/QLNV/View/DanhSachCanBo.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace QLNV.View
+{
+    partial class DanhSachCanBo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtDanhSanh = new System.Windows.Forms.DataGridView();
+            this.HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChucVu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaCanBo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtDanhSanh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtDanhSanh);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(563, 380);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dữ liệu";
+            // 
+            // dtDanhSanh
+            // 
+            this.dtDanhSanh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtDanhSanh.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.HoTen,
+            this.NgaySinh,
+            this.ChucVu,
+            this.MaCanBo});
+            this.dtDanhSanh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtDanhSanh.Location = new System.Drawing.Point(4, 19);
+            this.dtDanhSanh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtDanhSanh.Name = "dtDanhSanh";
+            this.dtDanhSanh.Size = new System.Drawing.Size(555, 357);
+            this.dtDanhSanh.TabIndex = 0;
+            this.dtDanhSanh.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtDanhSanh_CellContentClick);
+            // 
+            // HoTen
+            // 
+            this.HoTen.DataPropertyName = "HoTen";
+            this.HoTen.HeaderText = "Họ Tên";
+            this.HoTen.Name = "HoTen";
+            // 
+            // NgaySinh
+            // 
+            this.NgaySinh.DataPropertyName = "NgaySinh";
+            this.NgaySinh.HeaderText = "Ngày Sinh";
+            this.NgaySinh.Name = "NgaySinh";
+            // 
+            // ChucVu
+            // 
+            this.ChucVu.DataPropertyName = "ChucVu";
+            this.ChucVu.HeaderText = "Chức Vụ";
+            this.ChucVu.Name = "ChucVu";
+            // 
+            // MaCanBo
+            // 
+            this.MaCanBo.DataPropertyName = "MaCanBo";
+            this.MaCanBo.HeaderText = "Mã Cán Bộ";
+            this.MaCanBo.Name = "MaCanBo";
+            // 
+            // DanhSachCanBo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 410);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DanhSachCanBo";
+            this.Text = "Danh Sách Cán Bộ";
+            this.Load += new System.EventHandler(this.DanhSachCanBo_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtDanhSanh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dtDanhSanh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChucVu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaCanBo;
+    }
+}
